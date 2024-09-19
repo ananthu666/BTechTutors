ecb534402ee42df39bd89ec5c85e834bc1f583fd
Ananthu Jayakumar <115489088+ananthu666@users.noreply.github.com>
2024-09-19 22:46:57 +0530

    cors

--- BTech_Tutor_Backend/BTech_Tutor_Backend/settings.py
+++ BTech_Tutor_Backend/BTech_Tutor_Backend/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
+CSRF_TRUSTED_ORIGINS = []
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 
